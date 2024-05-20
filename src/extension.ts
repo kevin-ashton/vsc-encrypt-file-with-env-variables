17b021ef03c02b87bb03c5a53b77824de68c8ff7
Kevin M Ashton <kevin.m.ashton@gmail.com>
2024-05-19 22:58:00 -0600

    Decrypt

--- src/extension.ts
+++ src/extension.ts
@@ -2,27 +2,63 @@ import * as vscode from "vscode";
 import * as CryptoJS from "crypto-js";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("encrypt.file", async () => {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-      const document = editor.document;
-      const text = document.getText();
+  let encryptDisposable = vscode.commands.registerCommand(
+    "encrypt.file",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const document = editor.document;
+        const text = document.getText();
 
-      const password = "cat123";
-      const encrypted = CryptoJS.AES.encrypt(text, password).toString();
+        const password = "cat123";
+        const encrypted = CryptoJS.AES.encrypt(text, password).toString();
 
-      await editor.edit((editBuilder) => {
-        const start = new vscode.Position(0, 0);
-        const end = new vscode.Position(
-          document.lineCount - 1,
-          document.lineAt(document.lineCount - 1).text.length
-        );
-        editBuilder.replace(new vscode.Range(start, end), encrypted);
-      });
+        await editor.edit((editBuilder) => {
+          const start = new vscode.Position(0, 0);
+          const end = new vscode.Position(
+            document.lineCount - 1,
+            document.lineAt(document.lineCount - 1).text.length
+          );
+          editBuilder.replace(new vscode.Range(start, end), encrypted);
+        });
+      }
     }
-  });
+  );
 
-  context.subscriptions.push(disposable);
+  let decryptDisposable = vscode.commands.registerCommand(
+    "decrypt.file",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const document = editor.document;
+        const encryptedText = document.getText();
+
+        const password = "cat123";
+        let decrypted = "";
+        try {
+          const bytes = CryptoJS.AES.decrypt(encryptedText, password);
+          decrypted = bytes.toString(CryptoJS.enc.Utf8);
+        } catch (e) {
+          vscode.window.showErrorMessage(
+            "Failed to decrypt the file. It might not be encrypted with the correct password."
+          );
+          return;
+        }
+
+        await editor.edit((editBuilder) => {
+          const start = new vscode.Position(0, 0);
+          const end = new vscode.Position(
+            document.lineCount - 1,
+            document.lineAt(document.lineCount - 1).text.length
+          );
+          editBuilder.replace(new vscode.Range(start, end), decrypted);
+        });
+      }
+    }
+  );
+
+  context.subscriptions.push(encryptDisposable);
+  context.subscriptions.push(decryptDisposable);
 }
 
 export function deactivate() {}
